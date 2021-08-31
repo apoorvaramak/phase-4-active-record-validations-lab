0c7773a6c684a9cceb8579d4caf65ba4f229a3b9
Apoorva <apoorvar@comcast.net>
2021-08-30 20:56:39 -0500

    commit tree 892cd3415402bab95918f0524be427661c4e2a71

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :click_baity
+
+    def click_baity
+        if title 
+            unless title.include?("Won't Believe") || title.include?("Secret") || title.match?(/Top\s\d/) || title.include?("Guess")
+                errors.add(:title, "Not clickbiat-y enough")
+            end
+        end
+    end
 end
